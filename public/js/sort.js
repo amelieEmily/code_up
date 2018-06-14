c1cf3d9f3ad53ce6f3db080dea1d8feb701f63ce
Ming Tso <mwt16@ray01.doc.ic.ac.uk>
2018-06-15 00:49:57 +0100

    add event listener

--- public/js/sort.js
+++ public/js/sort.js
@@ -1,21 +1,23 @@
 
 var e = document.getElementById("input-sort");
-    var type = e.options[e.selectedIndex].value;
-    var data = {
-      username: $('#username').text(),
-      type: type
-    };
+e.addEventListener('change', function(event) {
+  var type = e.options[e.selectedIndex].value;
+  var data = {
+    username: $('#username').text(),
+    type: type
+  };
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    console.log(this.responseText);
-    document.write(this.responseText);
-  }
-};
-xhttp.open("POST", "/tutorList", true);
-xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-xhttp.send(JSON.stringify(data));
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      document.write(this.responseText);
+    }
+  };
+  xhttp.open("POST", "/tutorList", true);
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(JSON.stringify(data));
+})
 
 // $('#input-sort').on('change', function(event) {
 //     event.preventDefault();
